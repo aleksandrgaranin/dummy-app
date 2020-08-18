c18cd5dab63730502c53db9d9d3aaf7391296e0a
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-18 12:32:12 -0700

    with componentDidUpdate getting one element of data from request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import post from '../../components/Post/Post';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount(){
         axios.get('http://jsonplaceholder.typicode.com/posts')
@@ -22,13 +23,22 @@ class Blog extends Component {
                     }
                 });
                 this.setState({posts:updatedPosts});
-                //console.log(response);
+                //console.log(updatedPosts);
             });
         
     }
+
+    postSelectedHandler = (id)=> {
+        this.setState({selectedPostId: id});
+    }
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post 
+            key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={()=> this.postSelectedHandler(post.id)}
+            />
         })
         return (
             <div>
@@ -36,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
