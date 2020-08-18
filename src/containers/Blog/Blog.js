b64f8744d275b58a539fc5a63315b0a6c0bf2721
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-18 11:24:05 -0700

    connected to dummy json server with axios + connected to posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,25 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts:response.data})
+                //console.log(response);
+            });
+        
+    }
     render () {
+        const posts = this.state.posts.map(posts => {
+            return <Post title={posts.title}/>
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
