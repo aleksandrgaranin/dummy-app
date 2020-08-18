e4ec9a4183c57d32f1c572eb2cf30899d271f405
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-18 11:32:41 -0700

    modified data in componentDidMount

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
     state = {
@@ -13,14 +14,21 @@ class Blog extends Component {
     componentDidMount(){
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts:response.data})
+                const posts =response.data.slice(0, 4);
+                const updatedPosts = posts.map(post =>{
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts:updatedPosts});
                 //console.log(response);
             });
         
     }
     render () {
-        const posts = this.state.posts.map(posts => {
-            return <Post title={posts.title}/>
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}/>
         })
         return (
             <div>
