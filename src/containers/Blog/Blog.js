940740f1e9c15c337cc451e21930fea15fa4468d
aleksandGG <aleksandrgaranin89@gmail.com>
2020-08-28 15:53:29 -0700

    added swich + connected FullPost to Posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
-import { Route, Link } from 'react-router-dom';
-import NewPost from './NewPost/NewPost'
+import { Route, NavLink, Switch } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 
 import './Blog.css';
@@ -15,19 +16,31 @@ class Blog extends Component {
                      <header>
                         <nav>
                             <ul>
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to={{
+                                <li><NavLink 
+                                    to="/" 
+                                    exact
+                                    activeClassName="my-active"
+                                    activeStyle={{
+                                        color: '#fa923f',
+                                        textDecoration: 'underline'
+                                    }}
+                                >Home</NavLink></li>
+                                <li><NavLink to={{
                                     pathname: '/new-post',
                                     hash: '#submit',
                                     search: '?quick-submit=true'
-                                }}>New Post</Link></li>
+                                }}>New Post</NavLink></li>
                             </ul>
                         </nav>
                     </header>
                     {/* <Route path="/" exact render={()=> <Posts/>}/>
                     <Route path="/" render={()=> <h1>Home2</h1>}/> */}
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/new-post" component={NewPost}/>
+                    <Switch>
+                        <Route path="/" exact component={Posts}/>                    
+                        <Route path="/new-post" component={NewPost}/>
+                        <Route path="/:id" exact component={FullPost}/>
+                    </Switch>
+                    
                 </div>    
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Post from '../../../components/Post/Post'
-import axios from '../../../axios'
+import { link, Link } from 'react-router-dom';
+import Post from '../../../components/Post/Post';
+import axios from '../../../axios';
+import './Posts.css';
 
 class Posts extends Component {
     state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
+        posts: []        
     }
 
     componentDidMount(){
@@ -37,13 +37,15 @@ class Posts extends Component {
         let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>;
         if (!this.state.error){
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={()=> this.postSelectedHandler(post.id)
+                return (
+                <Link to={'/' + post.id} key={post.id}>
+                    <Post                         
+                        title={post.title} 
+                        author={post.author}
+                        clicked={()=> this.postSelectedHandler(post.id)
                 }
-            />
+                />
+                </Link>)
         })
         }
         return(
