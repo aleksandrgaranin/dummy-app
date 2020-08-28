60e0b865c3660766791fc4527b406415f467de0d
aleksandGG <aleksandrgaranin89@gmail.com>
2020-08-28 12:47:06 -0700

    added nav bar links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -51,6 +51,17 @@ class Blog extends Component {
          
         return (
             <div>
+                <div className="Blog">
+                     <header>
+                        <nav>
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/new-post">New Post</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                </div>
+               
                 <section className="Posts">
                     {posts}
                 </section>
