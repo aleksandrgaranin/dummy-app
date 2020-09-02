3d3e8b8731de5a52f4de6d5ce2ec32e7cbd32c90
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-01 17:48:47 -0700

    Nested Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import { Route, NavLink, Switch } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
-
-
 import './Blog.css';
 
 
@@ -17,14 +14,14 @@ class Blog extends Component {
                         <nav>
                             <ul>
                                 <li><NavLink 
-                                    to="/" 
+                                    to="/posts/"
                                     exact
                                     activeClassName="my-active"
                                     activeStyle={{
                                         color: '#fa923f',
                                         textDecoration: 'underline'
                                     }}
-                                >Home</NavLink></li>
+                                >Posts</NavLink></li>
                                 <li><NavLink to={{
                                     pathname: '/new-post',
                                     hash: '#submit',
@@ -36,9 +33,9 @@ class Blog extends Component {
                     {/* <Route path="/" exact render={()=> <Posts/>}/>
                     <Route path="/" render={()=> <h1>Home2</h1>}/> */}
                     <Switch>
-                        <Route path="/" exact component={Posts}/>                    
-                        <Route path="/new-post" component={NewPost}/>
-                        <Route path="/:id" exact component={FullPost}/>
+                        <Route path="/new-post"  component={NewPost}/>
+                        <Route path="/posts/"  component={Posts}/>                    
+                                                
                     </Switch>
                     
                 </div>    
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 import axios from '../../../axios';
 import './Posts.css';
 
@@ -29,7 +31,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id)=> {
-        this.props.history.push({pathname: '/' + id});
+        this.props.history.push({pathname: '/posts/' + id});
         // this.props.history.push('/' + id);
     }
 
@@ -38,7 +40,7 @@ class Posts extends Component {
         if (!this.state.error){
             posts = this.state.posts.map(post => {
                 return (
-                // <Link to={'/' + post.id} key={post.id}>
+                // <Link to={'/posts/' + post.id} key={post.id}>
                 <Post                         
                     title={post.title} 
                     author={post.author}
@@ -53,7 +55,8 @@ class Posts extends Component {
             <div>
                 <section className="Posts">
                     {posts}
-                </section>      
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost}/>      
             </div>             
         );
     }
